7e521d1bccacf378b06c52504a33a8d13dbcf346
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-07 15:39:05 -0800

    Initial Commit

---
+++ Line.rb
@@ -0,0 +1,11 @@
+=begin
+  Create a Line class.
+  User will initialize with two Points
+  Class will calculate the slope and intercept of this line.
+  First insiste that the line remain static after initialization.
+  Then allow the Line's Points to be edited after initialization.
+=end
+
+#Create a Line class
+
+#Create a Point class
